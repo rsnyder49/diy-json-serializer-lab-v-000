0f0fc627c61bb53793f7e1a5b974b52e125e4e73
Robert Snyder <r.snyder49@gmail.com>
2019-01-08 00:20:43 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,6 +2,10 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+  
+  def show
+    @product = Product.find(params[:id])
+  end
 
   def inventory
     product = Product.find(params[:id])
